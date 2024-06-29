b7e74bdea92037e8b6f7b8b94ec71390043ee171
DIFIKYT <ragexses@mail.ru>
2024-06-29 18:50:22 +0700

    Commit

--- Assets/Scripts/GameLogic/HealthBarFollow.cs
+++ Assets/Scripts/GameLogic/HealthBarFollow.cs
@@ -6,7 +6,7 @@ public class HealthBarFollow : MonoBehaviour
     [SerializeField] private Slider _healthBar;
     [SerializeField] private Vector3 _offset;
 
-    void Update()
+    private void Update()
     {
         if (_healthBar != null)
             _healthBar.transform.position = transform.position + _offset;
